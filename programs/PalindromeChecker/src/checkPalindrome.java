416c39c9732efb7edc7e32b2bf5d5a9b4dc372aa
roman <ronnoverro@gmail.com>
2023-09-22 20:17:23 -0700

    palindrome logic done

--- programs/PalindromeChecker/src/checkPalindrome.java
+++ programs/PalindromeChecker/src/checkPalindrome.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -12,10 +11,11 @@ public class checkPalindrome {
 
         char[] arr = new char[string1.length()];
 
+        boolean isPali = true;
+
         for (int i = 0; i < string1.length(); i++) {
             arr[i] = string1.charAt(i);
         };
-        // System.out.println(string1);
 
         // STACK:
         Stack <Character> string_stack = new Stack<Character>();
@@ -24,13 +24,39 @@ public class checkPalindrome {
         Queue<Character> string_queue = new LinkedList<Character>();
 
         for (int i = 0; i < string1.length(); i++) {
-            // arr[i] = string1.charAt(i);
             string_stack.add(string1.charAt(i));
             string_queue.add(string1.charAt(i));
         };
 
-        System.out.println(string_stack.toString());
-        System.out.println(string_queue.toString());
+        for (int i = 0; i < string1.length(); i++) {
+            // System.out.println(i + " " + string_stack.pop());
+            if (string_stack.pop() != string_queue.poll()) {
+                isPali = false;
+            }
+        }
+
+        if (isPali == false) {
+            System.out.println(string1 + " is not a palindrome");
+        } else {
+            System.out.println(string1 + " is a palindrome!");
+        }
+        // System.out.println(" ");
+        // for (int i = 0; i < string1.length(); i++) {
+        //     System.out.println(i + " " + string_queue.poll());
+        // }
+
+        // System.out.println(string_stack.toString());
+        // System.out.println(string_queue.toString());
+
+
+        // QUEUE METHODS:
+        // string_queue.poll() is for removing elements at the front. Also gets the value itself if added to a var
+        // string_queue.peek() gets the value without actually removing it. 
+
+        // STACK METHODS:
+        // string_stack.pop() is for removing most recently added element (at right).
+        // string_stack.peek() gets the value without actually removing it. 
+
 
         // [l, o, l]  -> STACK (FILO): start from right, to left. Keep adding on left. Remove at biggest index. 
         // -----+0
@@ -38,8 +64,6 @@ public class checkPalindrome {
         // 0----------+
 
         // TOPMOST ITEM IN A STACK IS AT THE FAR RIGHT OF IT LOL.
-
-        
         console.close();
     };
 };
