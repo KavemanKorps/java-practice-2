4e24efe5a8553112baca9f6d9a9367729c088898
roman <ronnoverro@gmail.com>
2023-11-29 18:59:14 -0800

    paliRecursion done

--- programs/paliRecursion/src/PaliRecursion.java
+++ programs/paliRecursion/src/PaliRecursion.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 public class PaliRecursion {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter text. Press Ctrl+D to stop:");
+        System.out.println("Enter text. Type 'end' to terminate program:");
 
-        // while there's input, get the current line and call "generatePali()" on it.
+        // while there's input, get the current line and call "generatePali()" on it:
         while (console.hasNextLine()) {
             String textLine = console.nextLine();
             String palindrome = generatePali(textLine);
             System.out.println("\n" + palindrome + "\n");
+
+            if (textLine.equals("end")) break;   
         }
         console.close();
     }
-    // recursive func. Recursion works by taking the first character.
-    private static String generatePali(String input) {
+    // recursive helper func. Recursion works by taking the first character. 
+    // made static b/c class itself needs to use it.
+    static String generatePali(String input) {
         // base case:
-        if (input.isEmpty()) return "";
+        if (input.isEmpty() == true) return "";
+        // get first char of string and keep moving it to the end of substring starting at index 1. 
         else {
-            char charOne = input.charAt(0);
-            return generatePali(input.substring(1)) + charOne;
+            char char1 = input.charAt(0);
+            return generatePali(input.substring(1)) + char1;
         }
     }
 }
